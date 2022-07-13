023d3f0374c73d0764d24c4dd099ff68a34e54ca
Sourcery AI <>
2022-07-13 10:35:27 +0000

    'Refactored by Sourcery'

--- rainbowow.py
+++ rainbowow.py
@@ -7,7 +7,7 @@ from termcolor import colored
 
 print("Enter text to be coloured red:")
 text = colored(input(), 'red')
-print("Coloured text: " + text)
+print(f"Coloured text: {text}")
 input()
 
 #colours = ['red', 'yellow', 'green', 'blue', 'magenta', 'cyan']
